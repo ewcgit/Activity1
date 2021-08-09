c0cbb1b2f607613e1286e06bd410e532c400e7fc
ewcgit <elijahwcov@gmail.com>
2021-08-09 01:57:04 -0400

    Add project files.

---
+++ Activity1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(textBox1.Text);
+            double n2 = Convert.ToDouble(textBox2.Text);
+
+            double n3 = n1 + n2;
+
+            textBox3.Text = n3.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(textBox1.Text);
+            double n2 = Convert.ToDouble(textBox2.Text);
+
+            double n3 = n1 - n2;
+
+            textBox3.Text = n3.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(textBox1.Text);
+            double n2 = Convert.ToDouble(textBox2.Text);
+
+            double n3 = n1 / n2;
+
+            textBox3.Text = n3.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(textBox1.Text);
+            double n2 = Convert.ToDouble(textBox2.Text);
+
+            double n3 = n1 * n2;
+
+            textBox3.Text = n3.ToString();
+        }
+    }
+}
